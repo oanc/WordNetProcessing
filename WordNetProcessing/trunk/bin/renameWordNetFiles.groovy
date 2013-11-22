69c465ec9cfae11597d1dd1744801383de580e25
frcascio <frcascio@f9835c53-d872-4803-a61e-d6c59fa17448>
2013-11-22 03:22:51 +0000

    adding files

---
+++ WordNetProcessing/trunk/bin/renameWordNetFiles.groovy
@@ -0,0 +1,119 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: frankcascio
+ * Date: 11/21/13
+ * Time: 8:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+ class renameWordNetFiles{
+
+
+     File inputDir ;
+     String suffix;
+
+
+     void run() {
+
+         inputDir.eachFile() { currentFile ->
+
+//             print "The current file is ${currentFile.toString()}  ";
+
+//             print "Path: ${currentFile.getParent().toString()}   ";
+
+             def path = currentFile.getParent().toString();
+             def name = currentFile.getName().toString();
+
+//             print "Name: ${name}";
+//             println "";
+
+             name = name.replaceAll(".xml", "-wn.xml");
+              name = name.replaceAll(".txt", "-wn.txt");
+
+//             print "newName is $newName";
+
+             def newFullName =  path+ "/" + name ;
+
+             println "newFullName is $newFullName";
+
+//             println "";
+
+             currentFile.renameTo(new File(newFullName.toString()))  ;
+
+//             println "currentFile is now ${currentFile.getName().toString()}";
+
+//             return;
+
+//             String oldFilename = "old.txt"
+//             String newFilename = "new.txt"
+
+//             new File(oldFilename).renameTo(new File(newFilename))
+
+
+
+         }
+
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+     public static void main(args){
+
+         if (args.size() != 2){
+             println "Usage: renameWordNetFiles /path/to/current/Files suffix"  ;
+             return;
+         }
+
+
+         def rn = new renameWordNetFiles();
+
+         rn.inputDir = new File(args[0]);
+         rn.suffix = args[0];
+
+         if(!rn.inputDir.exists()){
+             println "inputDir ${rn.inputDir.toString{}} not found";
+             return;
+         }
+
+
+         rn.run();
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
